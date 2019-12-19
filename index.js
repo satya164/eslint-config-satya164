11354942f674c85cc944403b2f242ebabe5d4f12
Satyajit Sahoo <satyajit.happy@gmail.com>
2019-12-19 22:34:32 +0100

    Add some rules for typescript

--- index.js
+++ index.js
@@ -257,16 +257,24 @@ module.exports = {
         },
       },
       rules: {
-        '@typescript-eslint/no-unused-vars': [
-          'error',
-          { argsIgnorePattern: '^_' },
-        ],
+        "@typescript-eslint/adjacent-overload-signatures": "error",
+        "@typescript-eslint/array-type": "error",
         '@typescript-eslint/consistent-type-assertions': [
           'error',
           { assertionStyle: 'as' },
         ],
+        "@typescript-eslint/member-delimiter-style": "error",
         '@typescript-eslint/no-array-constructor': 'error',
+        "@typescript-eslint/no-dynamic-delete": "error",
+        "@typescript-eslint/no-empty-interface": ["error", {"allowSingleExtends": true}],
+        "@typescript-eslint/no-extra-non-null-assertion": "error",
+        "@typescript-eslint/no-extraneous-class": "error",
+        "@typescript-eslint/no-misused-new": "error",
         '@typescript-eslint/no-namespace': 'error',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { argsIgnorePattern: '^_' },
+        ],
         '@typescript-eslint/no-use-before-define': [
           'error',
           {
@@ -276,6 +284,13 @@ module.exports = {
             typedefs: false,
           },
         ],
+        "@typescript-eslint/no-useless-constructor": "error",
+        "@typescript-eslint/prefer-for-of": "error",
+        "@typescript-eslint/prefer-function-type": "error",
+        "@typescript-eslint/prefer-namespace-keyword": "error",
+        "@typescript-eslint/prefer-optional-chain": "error",
+        "@typescript-eslint/triple-slash-reference": "error",
+        "@typescript-eslint/unified-signatures": "error",
 
         'default-case': 'off',
         'no-dupe-class-members': 'off',
